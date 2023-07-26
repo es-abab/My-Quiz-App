0989d8bdb6264af5bd080f7f94511e30dd377e40
Jerry <amlanujerry99@gmail.com>
2023-07-26 18:16:40 +0100

    updated nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,21 +15,22 @@ const Nav = ({ timeRemaining, onSubmit }) => {
   };
 
   return (
-    <div className='flex items-center justify-between h-[10vh] w-[100vw] px-[10vw] py-5 bg-slate-800'>
+    <div className='flex items-center justify-between h-[10vh] w-[100vw] px-[2vw] py-5 bg-slate-800'>
       {/* Move the "Quizzy" text and icon container to the extreme left */}
       <div className='flex items-center gap-3 text-white font-semibold text-lg'>
-        <div className='bg-white rounded-full w-8 h-8 flex justify-center items-center text-slate-800'>
-          {/* Add your custom Quizzy icon here */}
-        </div>
-        <div className="flex-grow">Quizzy</div>
+        {/* <div className='bg-white rounded-full w-8 h-8 flex justify-center items-center text-slate-800'>
+          Add your custom Quizzy icon here
+        </div> */}
+        <div className="flex-grow text-4xl font-bold text-orange-500">qui<span className='text-white'>zz</span>y</div>
       </div>
 
-      <div className='flex items-center gap-3 text-white font-semibold text-lg ml-auto'>
+      <div className='flex items-center gap-10 text-white font-semibold text-lg ml-auto'>
         {/* Search form */}
-        <form onSubmit={handleSearchSubmit} className='flex items-center gap-1'>
+        <form onSubmit={handleSearchSubmit} className='flex items-center gap-1  '>
+          <div className='w-[20rem] flex items-center px-2 py-1 border border-gray-300 rounded-full bg-slate-300 focus:outline-none focus:ring focus:ring-blue-300 text-sm'>
           <button
             type='submit'
-            className='bg-transparent border border-white rounded-md p-1 focus:outline-none focus:ring focus:ring-blue-300'
+            className='bg-transparent border  text-slate-900 rounded-md p-1 focus:outline-none '
           >
             <FaSearch size={14} />
           </button>
@@ -37,17 +38,21 @@ const Nav = ({ timeRemaining, onSubmit }) => {
             type='text'
             value={searchQuery}
             onChange={handleSearchChange}
-            className='px-2 py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-blue-300 text-sm'
+            className='px-2  text-lg bg-slate-300 text-slate-900 focus:outline-none focus:ring-none '
             placeholder='Search...'
           />
+          </div>
         </form>
         {/* End of search form */}
 
         {/* Esther Aboagyewa Abankwa */}
+        <div className='flex gap-2 border-l-2 border-l-white/20 pl-5'>
         <span className='bg-white rounded-full w-8 h-8 flex justify-center items-center text-slate-800'>
           <FaUserAlt size={18} />
         </span>
         Esther Aboagyewa Abankwa
+        </div>
+
       </div>
       <div className='flex items-center justify-center gap-10'>
         {/* Add other components in the gap-10 if needed */}
